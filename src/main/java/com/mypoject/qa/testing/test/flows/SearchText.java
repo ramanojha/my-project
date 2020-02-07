26dc95f6625386b35109968b641676b89f253451
Raman Ojha <ojha.raman99@gmail.com>
2020-02-08 02:07:55 +0530

    de-extending base class

--- src/main/java/com/mypoject/qa/testing/test/flows/SearchText.java
+++ src/main/java/com/mypoject/qa/testing/test/flows/SearchText.java
@@ -2,10 +2,9 @@ package com.mypoject.qa.testing.test.flows;
 
 import org.testng.annotations.Test;
 
-import com.myproject.qa.testing.framework.selenium.BaseWebDriver;
 import com.myproject.qa.testing.pages.GooglePageTest;
 
-public class SearchText extends BaseWebDriver{
+public class SearchText{
 
 	@Test(alwaysRun=true)
 	public static void enterTextInGoogleSearchBarTest() throws Exception{
